d5eb271fc3a9cd13aa88f19a845b1aff35cda657
Eduardo Wasem <eduardowasem@outlook.com>
2022-03-07 13:22:57 -0300

    Ajustes feitos na exibição dos episódios

--- app/src/main/java/br/com/ewapps/rickandmorty/ui/screen/CharacterDetailScreen.kt
+++ app/src/main/java/br/com/ewapps/rickandmorty/ui/screen/CharacterDetailScreen.kt
@@ -1,6 +1,7 @@
 package br.com.ewapps.rickandmorty.ui.screen
 
 import android.graphics.drawable.PaintDrawable
+import android.text.Layout
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
@@ -14,8 +15,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,127 +34,144 @@ fun CharacterDetailScreen(navController: NavController, characterData: Character
     Scaffold(topBar = {
         DetailTopAppBar(onBackPressed = { navController.popBackStack() })
     }) {
-        Card(
-            shape = RoundedCornerShape(20.dp), modifier = Modifier.padding(15.dp),
-            border = BorderStroke(1.dp, color = Color.LightGray), elevation = 6.dp
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Column(
+            Image(
+                painter = painterResource(id = characterData.image),
+                contentDescription = "Imagem do Personagem",
+                modifier = Modifier.fillMaxWidth(), contentScale = ContentScale.Crop
+            )
+
+            Card(
+                shape = RoundedCornerShape(20.dp),
                 modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState()),
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxWidth()
+                    .absoluteOffset(y = -15.dp)
             ) {
 
-                Image(
-                    painter = painterResource(id = characterData.image),
-                    contentDescription = "Imagem do Personagem",
-                    modifier = Modifier.size(360.dp)
-                )
-
-                Card(
-                    shape = RoundedCornerShape(20.dp), modifier = Modifier.fillMaxWidth().absoluteOffset(y = -15.dp)
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    Text(
+                        text = characterData.name,
+                        Modifier.padding(top = 10.dp),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Text(
+                        text = "Espécie: ${characterData.species}",
+                        Modifier.padding(top = 10.dp)
+                    )
+                    Text(text = "Gênero: ${characterData.gender}")
+                    Text(text = "Status: ${characterData.status}")
+                    Text(text = "Origem: ${characterData.origin?.name}")
+                    Text(text = "Localização: ${characterData.location?.name}")
+                    Text(
+                        text = "Lista de episódios:",
+                        Modifier.padding(top = 10.dp),
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp
+                    )
 
-                    Column(
+                    Card(
+                        shape = RoundedCornerShape(20.dp),
+                        backgroundColor = Color.Gray,
                         modifier = Modifier
-                            .fillMaxSize()
-                            ,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                            .fillMaxWidth()
+                            .padding(10.dp)
                     ) {
-                        Text(
-                            text = characterData.name,
-                            Modifier.padding(top = 10.dp),
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Text(
-                            text = "Espécie: ${characterData.species}",
-                            Modifier.padding(top = 10.dp)
-                        )
-                        Text(text = "Gênero: ${characterData.gender}")
-                        Text(text = "Status: ${characterData.status}")
-                        Text(text = "Origem: ${characterData.origin?.name}")
-                        Text(text = "Localização: ${characterData.location?.name}")
-                        Text(
-                            text = "Lista de episódios:",
-                            Modifier.padding(top = 10.dp),
-                            fontWeight = FontWeight.SemiBold,
-                            fontSize = 16.sp
-                        )
-                        for (i in characterData.episode?.indices!!) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            var tempAnt = "01"
+                            val list: MutableList<String> = mutableListOf()
+                            for (i in characterData.episode?.indices!!) {
 
-                            val temp =
-                                EpisodeData.getEpisode(characterData.episode[i].toInt()).season
-                            val epi =
-                                EpisodeData.getEpisode(characterData.episode[i].toInt()).episodes
-                            var tempAnt = ""
-                            if (i > 0) {
-                                tempAnt =
-                                    EpisodeData.getEpisode(characterData.episode[i - 1].toInt()).season
-                            }
+                                val temp =
+                                    EpisodeData.getEpisode(characterData.episode[i].toInt()).season
+                                val epi =
+                                    EpisodeData.getEpisode(characterData.episode[i].toInt()).episodes
 
-                            if (i == 0) {
-
-                                Text(
-                                    text = "Temporada $temp",
-                                    Modifier.padding(bottom = 8.dp, top = 8.dp),
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                                Card(
-                                    shape = MaterialTheme.shapes.medium,
-                                    backgroundColor = Color.LightGray,
-                                    elevation = 6.dp,
-                                    modifier = Modifier
-                                        .size(width = 300.dp, height = 40.dp)
-                                        .padding(5.dp)
-                                ) {
-                                    Column(
-                                        horizontalAlignment = Alignment.CenterHorizontally,
-                                        verticalArrangement = Arrangement.Center
-                                    ) {
-                                        Text(text = "Episódio $epi")
+                                if (temp == tempAnt) {
+                                    list.add(epi)
+                                } else {
+                                    if (list.isEmpty()) {
+                                        tempAnt = temp
+                                        list.clear()
+                                        list.add(epi)
+                                    } else {
+                                        Column(
+                                            modifier = Modifier
+                                                .fillMaxSize(),
+                                            horizontalAlignment = Alignment.CenterHorizontally
+                                        ) {
+                                            Text(
+                                                text = "Temporada $tempAnt",
+                                                color = Color.White,
+                                                fontWeight = FontWeight.SemiBold
+                                            )
+                                            Card(
+                                                shape = RoundedCornerShape(20.dp),
+                                                backgroundColor = Color.White,
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .padding(10.dp)
+                                            ) {
+                                                Column (modifier = Modifier
+                                                    .fillMaxSize(),
+                                                    horizontalAlignment = Alignment.CenterHorizontally){
+                                                    list.forEach {
+                                                        Text(text = "Episódio $it", Modifier.padding(3.dp) )
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        tempAnt = temp
+                                        list.clear()
+                                        list.add(epi)
                                     }
                                 }
-                            } else if (temp == tempAnt) {
-                                Card(
-                                    shape = MaterialTheme.shapes.medium,
-                                    backgroundColor = Color.LightGray,
-                                    elevation = 6.dp,
-                                    modifier = Modifier
-                                        .size(width = 300.dp, height = 40.dp)
-                                        .padding(5.dp)
-                                ) {
+                                if (characterData.episode.lastIndex == i){
                                     Column(
-                                        horizontalAlignment = Alignment.CenterHorizontally,
-                                        verticalArrangement = Arrangement.Center
+                                        modifier = Modifier
+                                            .fillMaxSize(),
+                                        horizontalAlignment = Alignment.CenterHorizontally
                                     ) {
-                                        Text(text = "Episódio $epi")
+                                        Text(
+                                            text = "Temporada $tempAnt",
+                                            color = Color.White,
+                                            fontWeight = FontWeight.SemiBold
+                                        )
+                                        Card(
+                                            shape = RoundedCornerShape(20.dp),
+                                            backgroundColor = Color.White,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .padding(10.dp)
+                                        ) {
+                                            Column (
+                                                modifier = Modifier
+                                                    .fillMaxSize(),
+                                                horizontalAlignment = Alignment.CenterHorizontally
+                                            ) {
+                                                list.forEach {
+                                                    Text(text = "Episódio $it")
+                                                }
+                                            }
+                                        }
                                     }
                                 }
-                            } else {
-                                Text(
-                                    text = "Temporada $temp",
-                                    Modifier.padding(bottom = 8.dp, top = 8.dp),
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.SemiBold,
-                                )
-                                Card(
-                                    shape = MaterialTheme.shapes.medium,
-                                    backgroundColor = Color.LightGray,
-                                    elevation = 6.dp,
-                                    modifier = Modifier
-                                        .size(width = 300.dp, height = 40.dp)
-                                        .padding(5.dp)
-                                ) {
-                                    Column(
-                                        horizontalAlignment = Alignment.CenterHorizontally,
-                                        verticalArrangement = Arrangement.Center
-                                    ) {
-                                        Text(text = "Episódio $epi")
-                                    }
-                                }
+
                             }
                         }
                     }
@@ -158,8 +179,8 @@ fun CharacterDetailScreen(navController: NavController, characterData: Character
             }
         }
     }
-
 }
+
 
 @Composable
 fun DetailTopAppBar(onBackPressed: () -> Unit = {}) {
@@ -170,7 +191,7 @@ fun DetailTopAppBar(onBackPressed: () -> Unit = {}) {
                 onClick = { onBackPressed() }) {
                 Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Botão voltar")
             }
-        })
+        }, elevation = 8.dp)
 }
 
 @Preview(showBackground = true)
